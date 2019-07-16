3b0517ded40f5219e9ed5ca6a509e091ed6a2914
OgiBalboa <48028006+OgiBalboa@users.noreply.github.com>
2019-07-16 10:06:46 +0300

    Update threading.py

--- threading.py
+++ threading.py
@@ -1,11 +1,11 @@
 import threading
 import time
-i = None
+c= 0
 def di():
   while 1:
     print("robot çalışıyor")
     time.sleep(1)
-    if i == 'q':  # Usin flag to stop loop
+    if i == 'q':
       print("HOOOOPPP")
       break
 
@@ -13,15 +13,42 @@ def dii():
   while 1:
     print("sensör çalışıyor")
     time.sleep(1)
-    if i == 'q' :   # Usin flag to stop loop
+    if i == 'q' :
       print("HOOOOPPP")
       break
-
+threads=[]
 dall = threading.Thread(target = di)
 dalla = threading.Thread(target = dii)
-while i != 'q': 
-  i = str(input(" Hangi dal ? (durdurmak için q ya basın)"))
-  if i == '1':
-    dall.start()
-  if i == '2':
+def main():
+ global c
+ global i
+ i = None
+ while i != 'q': 
+  try:
+   i = str(input(" Hangi dal ? (durdurmak için q ya basın)"))
+   if i == '1':
+     if c == 0:
+       threads.append(dall)
+       threads[c].start()
+     else :
+      threads.append(dall)
+      threads[c].join()
+   if i == '2':
     dalla.start()
+  except KeyboardInterrupt:
+   print(" HOOOOOOOOPP")
+   break
+j = None
+main()
+
+if i == 'q' and j != 's' :
+ try:
+  time.sleep(0.5)
+  j = str(input(" Ne yapalım ? R = reset S = Shut Down"))
+  if j== 'r':
+    c += 1
+    i = None
+    main()
+ except:
+   print("HATA")
+   raise
